ef4da765a83f7c63fa7224924b8315023d266230
Kazunori OYA <terje@oya3.net>
2024-01-26 09:44:05 +0900

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'vuetify/styles';  // vuetifyパレットカラーを利用するためにはGlobal CSSの”vuetify/styles”のimportが必要
+import { createVuetify } from 'vuetify';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify();
+createApp(App).use(vuetify).mount('#app');
